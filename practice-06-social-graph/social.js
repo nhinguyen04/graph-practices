af6f35a41728d2b53b5a060d7949617ffa65e5c3
nhinguyen04 <valentinetwin94@gmail.com>
2023-03-28 12:36:18 -0700

    social graph p1

--- practice-06-social-graph/social.js
+++ practice-06-social-graph/social.js
@@ -8,28 +8,48 @@ class SocialNetwork {
   }
 
   addUser(name) {
-    // Your code here
+    this.currentID++;
+    let newUser = {
+      id: this.currentID,
+      name: name
+    }
+
+    this.users[newUser.id] = newUser;
+    this.follows[newUser.id] = new Set();
+
+    return this.currentID;
   }
 
   getUser(userID) {
-    // Your code here
+    if (!this.users[userID]) return null;
+    else return this.users[userID];
   }
 
   follow(userID1, userID2) {
-    // Your code here
+    if (this.users[userID1] && this.users[userID2]) {
+      this.follows[userID1].add(userID2);
+      return true;
+    } else return false;
   }
 
   getFollows(userID) {
-    // Your code here
+    return this.follows[userID];
   }
 
   getFollowers(userID) {
-    // Your code here
+    let followers = new Set();
+
+    for (const [key, value] of Object.entries(this.follows)) {
+      if (value.has(userID)) followers.add(Number(key));
+    }
+
+    return followers;
   }
 
   getRecommendedFollows(userID, degrees) {
     // Your code here
   }
 }
+
 
 module.exports = SocialNetwork;
